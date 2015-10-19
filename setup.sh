1bb21c8a779bc162ba274b8efc11a42a5e1bfe89
Batandwa Colani <i@batandwa.me>
2015-10-19 02:46:58 +0200

    Updated setup code.

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
 apt-get install --yes nodejs
 apt-get install --yes build-essential
 npm install -g grunt-cli
+touch /code/.build-tool-set-up
